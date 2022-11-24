3f3bb1d63caf67edf386988c3e25411921b06c2b
Virgonb1 <118275437+Virgonb1@users.noreply.github.com>
2022-11-24 15:51:59 +0700

    first commit

---
+++ bin/bai_4.dart
@@ -0,0 +1,7 @@
+void main() {
+  String str =
+      " đây là kết quả của buổi học thứ 2 về dart: dart basics (phần 1)...";
+
+  print(
+      "Đây là ${str.substring(8, 19)} ${str.substring(20, 37)} Dart: DART BASIC ${str.substring(56, 64)}");
+}
---
+++ test/bai_4_test.dart
@@ -0,0 +1,8 @@
+import 'package:bai_4/bai_4.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
